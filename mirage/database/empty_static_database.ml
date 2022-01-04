1b9b18efe2d34939d8315fefd5147539d9bf7333
Carine Morel <carine@tarides.com>
2022-01-04 15:46:16 +0100

    Precomputed database.

--- mirage/database/empty_static_database.ml
+++ mirage/database/empty_static_database.ml
@@ -1,7 +1,4 @@
-open Bayesian.Database
 
 let bayesianBody = Bayesian.Database.create ()
-
-open Bayesian.Database
 
 let bayesianMainSubject = Bayesian.Database.create ()
---
+++ mirage/database/static_database.ml
@@ -0,0 +1,4 @@
+open Bayesian.Database
+let bayesianBody = { nb_spam = 467; nb_ham = 2533; freqs = BayesianBody_012.db}
+open Bayesian.Database
+let bayesianMainSubject = { nb_spam = 467; nb_ham = 2533; freqs = BayesianMainSubject_000.db}
