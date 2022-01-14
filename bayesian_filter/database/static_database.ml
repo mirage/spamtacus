6a280ae1594155d58a48ae24870f5abe94e70ab3
Carine Morel <carine@tarides.com>
2022-01-14 21:24:48 +0100

    Database update to accomodate BagOfWords changes.

--- bayesian_filter/database/static_database.ml
+++ bayesian_filter/database/static_database.ml
@@ -1,2 +1,2 @@
-let bayesianBody = Database.{ nb_spam = 467; nb_ham = 2533; freqs = BayesianBody_012.db}
+let bayesianBody = Database.{ nb_spam = 467; nb_ham = 2533; freqs = BayesianBody_005.db}
 let bayesianMainSubject = Database.{ nb_spam = 467; nb_ham = 2533; freqs = BayesianMainSubject_000.db}
