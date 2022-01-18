8e17b260cb31bbfd088c8aa5d02ce6add4cd09ee
Carine Morel <carine@tarides.com>
2022-01-18 22:11:19 +0100

    Add documentation for the new feature.

--- bayesian_filter/features.mli
+++ bayesian_filter/features.mli
@@ -1,3 +1,22 @@
 module BayesianBody : Spamtacus.FEATURE with type db = Database.db
+(** Naive Bayesian filter on mail body content. *)
+
 module BayesianSubject : Spamtacus.FEATURE with type db = Database.db
+(** Naive Bayesian filter on subject header value. *)
+
 module BasicAntiVirus : Spamtacus.FEATURE
+(** Basic antivirus that  rejects mails with forbidden attachment. It looks for
+  attachment file name in  :
+  {ul
+  {- [name] parameter of [content-type] headers}
+  {- [filename] parameter of [content-disposition] headers}}
+    in each part of an incoming mail. If the attachement filenames found have 
+  a forbidden extension  the mail is given a rank of [1.].
+ 
+  Forbidden extensions are [.ade], [.adp], [.bat], [.chm], [.cmd],
+   [.com], [.cpl], [.exe], [.hta], [.ins], [isp], [.jse], [.lib],
+   [.mde], [.msc], [.msp], [.mst], [.pif], [.scr], [.sct], [shb],
+   [.sys], [.vb], [.vbe], [.vbs], [.vxd], [.wsc], [.wsf], [.wsh].  
+
+  Note: This feature does not require any machine learning algorithm nor
+ a database. *)
--- bayesian_filter/spamtacus_bayesian.mli
+++ bayesian_filter/spamtacus_bayesian.mli
@@ -1,21 +1,45 @@
 open Spamtacus
-module BayesianBody : FEATURE
-module BayesianSubject : FEATURE
-module BasicAntiVirus : FEATURE
 
-(** This filter uses two features:
+(** This filter uses tree features:
   {ul
-    {- a naive bayesian algorithm that works on mail body. }
-    {- a naive bayesian algorithm that works on words contained
-    in [subject] header value. }}
+    {- a naive bayesian algorithm that works on mail body (See {!BayesianBody}). }
+    {- a naive bayesian algorithm that works on [subject] header value (See {!BayesianSubject}). }
+    {- a basic antivirus that rejects mails with forbidden attachment (`.exe` files
+     for example) (See {!BasicAntiVirus}). }}
 
-    The decision tree of this filter is quite simple: both features return
-    a probability of being a spam. The label is:
-    {ul
-    {- [`Spam] if any of the features returns a probability higher than 0.7}
-    {- [`Ham] otherwise.}}
-     
+    The decision tree of this filter is quite simple:   
+   {ul
+    {- a mail is labelled as [`Spam] if it contains a forbidden attachment or if
+    the probability of beeing a spam computed by any of the two bayesian filters
+    is greater than 0.7,}
+    {- if no forbidden attachment has been found and both probabilities of beeing a
+    spam are lower than 0.3, it is labelled as [`Ham`].}
+    {- Otherwise an `Unknown` label is returned.}}
 *)
+
+(** {1:bayesian_features Implemented features *)
+module BayesianBody : Spamtacus.FEATURE with type db = Database.db
+(** Naive Bayesian filter on mail body content. *)
+
+module BayesianSubject : Spamtacus.FEATURE with type db = Database.db
+(** Naive Bayesian filter on subject header value. *)
+
+module BasicAntiVirus : Spamtacus.FEATURE
+(** Basic antivirus that  rejects mails with forbidden attachment. It looks for
+  attachment file name in  :
+  {ul
+  {- [name] parameter of [content-type] headers}
+  {- [filename] parameter of [content-disposition] headers}}
+    in each part of an incoming mail. If the attachement filenames found have 
+  a forbidden extension  the mail is given a rank of [1.].
+ 
+  Forbidden extensions are [.ade], [.adp], [.bat], [.chm], [.cmd],
+   [.com], [.cpl], [.exe], [.hta], [.ins], [isp], [.jse], [.lib],
+   [.mde], [.msc], [.msp], [.mst], [.pif], [.scr], [.sct], [shb],
+   [.sys], [.vb], [.vbe], [.vbs], [.vxd], [.wsc], [.wsf], [.wsh].  
+
+  Note: This feature does not require any machine learning algorithm nor
+ a database. *)
 
 val train_and_write_to_file : training_set -> output:Fpath.t -> unit
 (** [train_and_write_to_file training_set ~output] computes the
